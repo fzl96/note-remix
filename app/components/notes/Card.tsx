0289f9a66b673ac05649f927693b0089573376e5
seika333 <fpsdragon333@gmail.com>
2022-07-16 01:50:20 +0700

    add pwa

--- app/components/notes/Card.tsx
+++ app/components/notes/Card.tsx
@@ -1,16 +1,35 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Divider, useMantineColorScheme } from "@mantine/core";
 import type { Note } from "@prisma/client";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { TiStarOutline } from "react-icons/ti";
 import { format } from "date-fns";
 import { Menu } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 interface Props {
   note: Note;
+  index: number;
 }
 
-const Card = ({ note }: Props) => {
+const easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+
+const Card = ({ note, index }: Props) => {
   const { colorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
 
@@ -19,12 +38,10 @@ const Card = ({ note }: Props) => {
   return (
     <>
       <motion.div
-        whileHover={{ scale: 1.03 }}
-        whileTap={{ scale: 1 }}
+        variants={fadeInUp}
         className={`${
           dark ? "bg-grayishDark" : "bg-white"
-        } p-5  rounded-lg shadow-sm lg:h-full`}
-        layoutId={note.id.toString()}
+        } p-5 rounded-lg shadow-sm lg:h-full`}
       >
         <div className="flex flex-col h-full">
           <div className="flex items-center justify-end gap-2 pb-2 text-slate-400">
@@ -32,7 +49,12 @@ const Card = ({ note }: Props) => {
             <Menu className={`rounded-2xl ${dark ? "bg-grayishDark" : ""}`}>
               {noteMenuItem.map((item: string, index: number) => {
                 return (
-                  <Menu.Item className={`${dark ? "hover:bg-grayishDark2" : "hover:bg-gray"}`} key={index}>
+                  <Menu.Item
+                    className={`${
+                      dark ? "hover:bg-grayishDark2" : "hover:bg-grayish"
+                    }`}
+                    key={index}
+                  >
                     {item}
                   </Menu.Item>
                 );
@@ -40,18 +62,20 @@ const Card = ({ note }: Props) => {
             </Menu>
           </div>
           <div className="flex flex-col justify-between h-full">
-            <div>
-              <motion.h3
-                className={`lg:text-lg text-sm font-semibold ${
-                  dark ? "" : "text-[#000f36]"
-                }`}
-              >
-                {note.title}
-              </motion.h3>
-              <motion.p className="lg:text-sm text-xs">
-                {note.body.slice(0, 50)}...
-              </motion.p>
-            </div>
+            <Link to={`/app/notes/${note.id}`}>
+              <div>
+                <motion.h3
+                  className={`lg:text-lg text-sm font-semibold ${
+                    dark ? "" : "text-[#000f36]"
+                  }`}
+                >
+                  {note.title}
+                </motion.h3>
+                <motion.p className="lg:text-sm text-xs">
+                  {note.body.slice(0, 50)}...
+                </motion.p>
+              </div>
+            </Link>
             <div className="h-5" />
             <div>
               <div className="flex items-center gap-3">
--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useLocation, useMatches } from "@remix-run/react";
 import type { ColorScheme } from "@mantine/core";
 import type { MetaFunction } from "@remix-run/node";
 import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
@@ -11,41 +13,75 @@ import {
 } from "@remix-run/react";
 import { useState } from "react";
 import styles from "./tailwind.css";
-
+import { AnimatePresence } from "framer-motion";
+let isMount = true;
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
-
 export default function App() {
+  let location = useLocation();
+  let matches = useMatches();
+
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
+
   return (
     <html lang="en">
       <head>
-        <Meta />
-        <Links />
+        <link rel="manifest" href="/resources/manifest.webmanifest" />
+        <Meta /> <Links />
       </head>
       <body className="font-man">
-        <MantineTheme>
-          <Outlet />
-        </MantineTheme>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <AnimatePresence exitBeforeEnter>
+          <MantineTheme>
+            <Outlet />
+          </MantineTheme>
+        </AnimatePresence>
+        <ScrollRestoration /> <Scripts /> <LiveReload />
       </body>
     </html>
   );
 }
-
 function MantineTheme({ children }: { children: React.ReactNode }) {
   const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-
   return (
     <ColorSchemeProvider
       colorScheme={colorScheme}
